97ce988d11c861a4b09c94033107864f816f5850
Robert Wassens <robert.wassens@technolution.nl>
2023-12-15 14:27:02 +0100

    day 12 en 13

---
+++ day12/part2.py
@@ -0,0 +1,86 @@
+import os
+BLUE = '\033[94m'
+CYAN = '\033[96m'
+GREEN = '\033[92m'
+ORANGE = '\033[93m'
+RED = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+def string_to_ints(str, separator):
+    s = str.strip().split(separator)
+    a = []
+    for n in s:
+        a.append(int(n.strip()))
+    return a
+cnt=0
+def get_options(s, n, l):
+    # print(l*" ", s,n)
+    so,no = s,n
+    h=0
+    global cnt
+    for i,c in enumerate(s):
+        if c == "#" or (c == "?" and h>0):
+            if h == 0:
+                st=c
+            h +=1
+        elif c == "?":
+            # print(l*" ","? as .")
+            get_options(s[i+1:], n, l+1) # treat a dot
+            assert(h==0)
+            h+=1 # treat as #
+            st=c
+        elif h>0:
+            assert(c==".")
+            return 0
+        else:
+            assert(c==".")
+            # print(l*" ","next is .")
+            get_options(s[i+1:], n, l+1) # treat a dot
+            return
+
+        if h == n[0]:
+            if len(n) == 1: # found last set
+                if i == len(s)-1 or not "#" in s[i+1:]:
+                    cnt +=1
+                    # print(l*" ", "found", cnt)
+                    return  # Last set in line
+                else:
+                    return # This is not an option
+            if i == len(s)-1:
+                return
+            elif s[i+1] == "#":
+                return
+            else: # next is . or ?
+                # print(l*" ","next ? or .")
+                get_options(s[i+2:], n[1:], l+1)
+                return 
+ 
+dir = os.path.dirname(__file__)
+f = open(dir+"/input.txt")
+ns=[]
+ss=[]
+repeat=5
+for l in f.readlines():
+    s = l.strip().split(" ")
+    ns.append(string_to_ints(s[1],",")*repeat)
+    t = s[0].replace("..", ".")
+    while t != s[0]:
+        s[0] = t
+        t = s[0].replace("..", ".")
+    t = t + '?'
+    t=t*repeat
+    ss.append(t[0:-1])
+    
+sum = 0
+for i,s in enumerate(ss):
+    cnt = 0
+    get_options(s, ns[i],0)
+    print(s,ns[i], cnt)
+    sum += cnt
+    
+print(sum)
+    
+    
+    
---
+++ day13/part1.py
@@ -0,0 +1,84 @@
+import os
+BLUE = '\033[94m'
+CYAN = '\033[96m'
+GREEN = '\033[92m'
+ORANGE = '\033[93m'
+RED = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+def string_to_ints(str, separator):
+    s = str.strip().split(separator)
+    a = []
+    for n in s:
+        a.append(int(n.strip()))
+    return a
+
+def line_to_bin(l:str):
+    l=l.replace("#","1")
+    l=l.replace(".","0")
+    return int(l,2)
+
+def is_mirror(a, l,r, fixed):
+    b=bin(abs(a[l]-a[r])).count('1') == 1
+    if a[l]==a[r] or (b and not fixed):
+        if b:
+            print(l, bin(a[l]), r, bin(a[r]))
+            fixed=True
+        if l > 0 and r < len(a)-1:
+            return is_mirror(a,l-1, r+1, fixed)
+        else:
+            return fixed
+    return False
+
+def find_mirror(lines):
+    lb =[]
+    cb=[]
+    sum=0
+    for l in lines:
+        lb.append(line_to_bin(l))
+        
+    for i in range(len(lines[0])):
+        s = ""
+        for l in lines:
+            s+=l[i]  
+        cb.append(line_to_bin(s))
+
+    for i in range(len(lines)-1):
+        print(i)
+        if is_mirror(lb, i, i+1, False):
+            n = i+1
+            print(" ", n*100)
+            sum += n * 100
+            break
+    for i in range(len(lines[0])-1):
+        print(i)
+        if is_mirror(cb, i, i+1, False):
+            n = i+1
+            print(" ", n)
+            sum += n
+            break
+    return sum
+
+dir = os.path.dirname(__file__)
+f = open(dir+"/input.txt")
+lines = []
+sum = 0
+for l in f.readlines():
+    l=l.strip()
+    if l=="":
+        n=find_mirror(lines)  
+        sum += n   
+        print(n)
+        lines=[]
+    else:
+        lines.append(l.strip())
+    print(l)
+    
+
+        
+print(sum)
+    
+    
+    
